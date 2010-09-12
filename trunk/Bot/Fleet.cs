4958b5eb14d33d13e435a7462b34fb8770a46dee
IceHar <IceHar@b33c8b65-c307-4ce9-5f5a-685a51bbcf59>
2010-09-12 20:43:41 +0000

    New functions in BaseBot: distance check functions, fleet functions, etc.

--- trunk/Bot/Fleet.cs
+++ trunk/Bot/Fleet.cs
@@ -1,83 +1,101 @@
-public class Fleet {
-    // Initializes a fleet.
-    public Fleet(int owner,
-		 int numShips,
-		 int sourcePlanet,
-		 int destinationPlanet,
-		 int totalTripLength,
-		 int turnsRemaining) {
-	this.owner = owner;
-	this.numShips = numShips;
-	this.sourcePlanet = sourcePlanet;
-	this.destinationPlanet = destinationPlanet;
-	this.totalTripLength = totalTripLength;
-	this.turnsRemaining = turnsRemaining;
-    }
+namespace Bot
+{
+	public class Fleet
+	{
+		// Initializes a fleet.
+		public Fleet(int owner,
+		             int numShips,
+		             int sourcePlanet,
+		             int destinationPlanet,
+		             int totalTripLength,
+		             int turnsRemaining)
+		{
+			this.owner = owner;
+			this.numShips = numShips;
+			this.sourcePlanet = sourcePlanet;
+			this.destinationPlanet = destinationPlanet;
+			this.totalTripLength = totalTripLength;
+			this.turnsRemaining = turnsRemaining;
+		}
 
-    // Initializes a fleet.
-    public Fleet(int owner,
-		 int numShips) {
-	this.owner = owner;
-	this.numShips = numShips;
-	this.sourcePlanet = -1;
-	this.destinationPlanet = -1;
-	this.totalTripLength = -1;
-	this.turnsRemaining = -1;
-    }
+		// Initializes a fleet.
+		public Fleet(int owner,
+		             int numShips)
+		{
+			this.owner = owner;
+			this.numShips = numShips;
+			sourcePlanet = -1;
+			destinationPlanet = -1;
+			totalTripLength = -1;
+			turnsRemaining = -1;
+		}
 
-    // Accessors and simple modification functions. These should be mostly
-    // self-explanatory.
-    public int Owner() {
-	return owner;
-    }
+		// Accessors and simple modification functions. These should be mostly
+		// self-explanatory.
+		public int Owner()
+		{
+			return owner;
+		}
 
-    public int NumShips() {
-	return numShips;
-    }
+		public int NumShips()
+		{
+			return numShips;
+		}
 
-    public int SourcePlanet() {
-	return sourcePlanet;
-    }
+		public int SourcePlanet()
+		{
+			return sourcePlanet;
+		}
 
-    public int DestinationPlanet() {
-	return destinationPlanet;
-    }
+		public int DestinationPlanet()
+		{
+			return destinationPlanet;
+		}
 
-    public int TotalTripLength() {
-	return totalTripLength;
-    }
+		public int TotalTripLength()
+		{
+			return totalTripLength;
+		}
 
-    public int TurnsRemaining() {
-	return turnsRemaining;
-    }
+		public int TurnsRemaining()
+		{
+			return turnsRemaining;
+		}
 
-    public void RemoveShips(int amount) {
-	numShips -= amount;
-    }
+		public void RemoveShips(int amount)
+		{
+			numShips -= amount;
+		}
 
-    // Subtracts one turn remaining. Call this function to make the fleet get
-    // one turn closer to its destination.
-    public void TimeStep() {
-	if (turnsRemaining > 0) {
-	    --turnsRemaining;
-	} else {
-	    turnsRemaining = 0;
-	}
-    }
+		// Subtracts one turn remaining. Call this function to make the fleet get
+		// one turn closer to its destination.
+		public void TimeStep()
+		{
+			if (turnsRemaining > 0)
+			{
+				--turnsRemaining;
+			}
+			else
+			{
+				turnsRemaining = 0;
+			}
+		}
 
-    private int owner;
-    private int numShips;
-    private int sourcePlanet;
-    private int destinationPlanet;
-    private int totalTripLength;
-    private int turnsRemaining;
-	
-	private Fleet(Fleet _f) {
-		owner = _f.owner;
-		numShips = _f.numShips;
-		sourcePlanet = _f.sourcePlanet;
-		destinationPlanet = _f.destinationPlanet;
-		totalTripLength = _f.totalTripLength;
-		turnsRemaining = _f.turnsRemaining;
+		private readonly int owner;
+		private int numShips;
+		private readonly int sourcePlanet;
+		private readonly int destinationPlanet;
+		private readonly int totalTripLength;
+		private int turnsRemaining;
+
+		private Fleet(Fleet f)
+		{
+			owner = f.owner;
+			numShips = f.numShips;
+			sourcePlanet = f.sourcePlanet;
+			destinationPlanet = f.destinationPlanet;
+			totalTripLength = f.totalTripLength;
+			turnsRemaining = f.turnsRemaining;
+		}
 	}
 }
--- trunk/Bot/Planet.cs
+++ trunk/Bot/Planet.cs
@@ -1,73 +1,90 @@
-public class Planet {
-    // Initializes a planet.
-    public Planet(int planetID,
-                  int owner,
-		  int numShips,
-		  int growthRate,
-		  double x,
-		  double y) {
-	this.planetID = planetID;
-	this.owner = owner;
-	this.numShips = numShips;
-	this.growthRate = growthRate;
-	this.x = x;
-	this.y = y;
-    }
+namespace Bot
+{
+	public class Planet
+	{
+		// Initializes a planet.
+		public Planet(int planetID,
+		              int owner,
+		              int numShips,
+		              int growthRate,
+		              double x,
+		              double y)
+		{
+			this.planetID = planetID;
+			this.owner = owner;
+			this.numShips = numShips;
+			this.growthRate = growthRate;
+			this.x = x;
+			this.y = y;
+		}
 
-    // Accessors and simple modification functions. These should be mostly
-    // self-explanatory.
-    public int PlanetID() {
-	return planetID;
-    }
+		// Accessors and simple modification functions. These should be mostly
+		// self-explanatory.
+		public int PlanetID()
+		{
+			return planetID;
+		}
 
-    public int Owner() {
-	return owner;
-    }
+		public int Owner()
+		{
+			return owner;
+		}
 
-    public int NumShips() {
-	return numShips;
-    }
+		public int NumShips()
+		{
+			return numShips;
+		}
 
-    public int GrowthRate() {
-	return growthRate;
-    }
+		public int GrowthRate()
+		{
+			return growthRate;
+		}
 
-    public double X() {
-	return x;
-    }
+		public double X()
+		{
+			return x;
+		}
 
-    public double Y() {
-	return y;
-    }
+		public double Y()
+		{
+			return y;
+		}
 
-    public void Owner(int newOwner) {
-	this.owner = newOwner;
-    }
+		public void Owner(int newOwner)
+		{
+			owner = newOwner;
+		}
 
-    public void NumShips(int newNumShips) {
-	this.numShips = newNumShips;
-    }
+		public void NumShips(int newNumShips)
+		{
+			numShips = newNumShips;
+		}
 
-    public void AddShips(int amount) {
-	numShips += amount;
-    }
+		public void AddShips(int amount)
+		{
+			numShips += amount;
+		}
 
-    public void RemoveShips(int amount) {
-	numShips -= amount;
-    }
+		public void RemoveShips(int amount)
+		{
+			numShips -= amount;
+		}
 
-    private int planetID;
-    private int owner;
-    private int numShips;
-    private int growthRate;
-    private double x, y;
+		private readonly int planetID;
+		private int owner;
+		private int numShips;
+		private readonly int growthRate;
+		private readonly double x;
+		private readonly double y;
 
-    private Planet (Planet _p) {
-	planetID = _p.planetID;
-	owner = _p.owner;
-	numShips = _p.numShips;
-	growthRate = _p.growthRate;
-	x = _p.x;
-	y = _p.y;
-    }
+		private Planet(Planet p)
+		{
+			planetID = p.planetID;
+			owner = p.owner;
+			numShips = p.numShips;
+			growthRate = p.growthRate;
+			x = p.x;
+			y = p.y;
+		}
+	}
 }
