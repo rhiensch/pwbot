0f5dd5c839e5b251ca425aa0f58f9a9ba6e9c857
IceHar <IceHar@b33c8b65-c307-4ce9-5f5a-685a51bbcf59>
2010-11-12 16:08:44 +0000

    Changed attack mode in AntiCrisisAdviser. Moves selection by best movesSets combinations

--- tournament.ps1
+++ tournament.ps1
@@ -1,9 +1,6 @@
 #CSharpEngine.exe "maps/map41.txt" 2000 100 "log.txt" "Bot.exe" "java -jar example_bots/DualBot.jar" | java -jar tools/ShowGame.jar
 
-#$root_dir = "C:\Projects\pw\"
-#$root_dir = "C:\war\"
-
-
+$root_dir = "C:\Projects\pw\"
 $my_bot = "bot.exe"
 #$enemy_bot = "bot_exe\bot3.exe"
 
@@ -14,15 +11,20 @@ $my_bot = "bot.exe"
 $bots = @()
 #$bots += "bot_exe\bot1.exe"
 #$bots += "bot_exe\bot2.exe"
-$bots += "bot_exe\bot3.exe"
-$bots += "bot_exe\bot4.exe"
+#$bots += "bot_exe\bot3.exe"
+#$bots += "bot_exe\bot4.exe"
 #$bots += "bot_exe\bot5.exe"
+#$bots += "bot_exe\bot6.exe"
 #$bots += '"java -jar example_bots/RageBot.jar"'
 #$bots += '"java -jar example_bots/DualBot.jar"'
 #$bots += '"java -jar example_bots/BullyBot.jar"'
 #$bots += '"java -jar example_bots/ProspectorBot.jar"'
 #$bots += '"java -jar example_bots/RandomBot.jar"'
-$bots += "bot_exe\HazardBot_12.exe"
+#$bots += "bot_exe\HazardBot_12.exe"
+#$bots += "bot_exe\HazardBot_9.exe"
+#$bots += "bot_exe\HazardBot_10.exe"
+#$bots += "bot_exe\HazardBot_11.exe"
+$bots += "bot_exe\Torquemada 3.exe"
 #$bots += "bot_exe\mubot.exe"
 #$bots += "bot_exe\BlueSky9.exe"
 
@@ -36,12 +38,12 @@ foreach($bot in $bots) {
     $maps_played=0
     
     foreach($i in (1..100)) {   
-	if ($botname -match "java") {
+	#if ($botname -match "java") {
 		$output = java.exe -jar tools\PlayGame.jar maps\map$i.txt 3000 200 log.txt $my_bot "$botname" 2>&1
-	}
-	else {
-        	$output = .\CSharpEngine.exe "maps\map$i.txt" 3000 200 "log.txt" $my_bot $botname 2>&1
-	}
+	#}
+	#else {
+        #	$output = .\CSharpEngine.exe "maps\map$i.txt" 3000 200 "log.txt" $my_bot $botname 2>&1
+	#}
         
         if($output -match "Player 1 Wins!") {
             $player_1_counter = $player_1_counter + 1
